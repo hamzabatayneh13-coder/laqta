a2fd5f0e249475463b3d2840ca3bc5b35789670f
hamza <bataynehh03@gmail.com>
2026-04-20 05:40:06 +0300

    Debug DATABASE_URL at runtime

--- my-project/prisma/prisma.service.ts
+++ my-project/prisma/prisma.service.ts
@@ -8,13 +8,16 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
     const connectionString = process.env.DATABASE_URL;
 
-    if (!connectionString) {
-      throw new Error('DATABASE_URL is missing');
-    }
+    console.log('DATABASE_URL exists?', !!connectionString);
+    console.log(
+      'DATABASE_URL host snippet:',
+      connectionString?.split('@')?.[1]?.split('/')?.[0],
+    );
+
+    if (!connectionString) throw new Error('DATABASE_URL is missing');
 
     const pool = new Pool({ connectionString });
     const adapter = new PrismaPg(pool);
-
     super({ adapter });
   }
 
